6f4f81708789e57d2c7c3f183b1ce211721e7311
ahmedessam228 <ahmedeliwa228@gmail.com>
2023-08-11 02:33:22 +0300

    atef

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 print("Ahmed")
 print("Ahmed hiiiiiiiii")
 print("Ahmed Essam Eliwa عايز حد يركبه وبيحب واحده مش عارفه انو عايش اصلا ويارب طيظك تهدى شويه عشان كدا عيب")
+print("عيب ياعااطف انت مش صغير ")
