d2eea4c20c774d3b0ae6df4304c785dfb388c974
ahmedsaadtaha <ahmedsdzyadt442@gmail.com>
2023-08-11 02:46:00 +0300

    amhedsaad

--- test.py
+++
@@ -1,5 +0,0 @@
-print("Ahmed")
-print("Ahmed hiiiiiiiii")
-print("Ahmed Essam Eliwa عايز حد يركبه وبيحب واحده مش عارفه انو عايش اصلا ويارب طيظك تهدى شويه عشان كدا عيب")
-print("Ya Ged3an 3eb ya ged3aaaan")
-print("عيب ياعااطف انت مش صغير ")
