23abb334c4af12c308fee662f9f466ec1ae581bc
AhmedSarg <xsarg22@gmail.com>
2023-08-18 22:27:50 +0300

    cleaning code

---
+++ login_screen.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Login")
+root.resizable(True,True)
+root.config(background="#ffeedb")
+label = Label(root, text = "Login Screen Test")
+label.pack()
+root.state("zoomed")
+root.mainloop()
--- splash_screen.py
+++ splash_screen.py
@@ -4,9 +4,24 @@ from tkinter.ttk import Progressbar
 import time
 import os
 
+
+def login_screen():
+    
+    splash.destroy()
+    
+    root = Tk()
+    root.title("Login")
+    root.resizable(True,True)
+    root.config(background="#ffeedb")
+    label = Label(root, text = "Login Screen Test")
+    label.pack()
+    root.state("zoomed")
+    root.mainloop()
+
 splash = Tk()
 splash.title("Splash screen!")
-
+label = Label(splash, text = "Splash Screen Test")
+label.pack()
 screen = Tk()
 screenWidth = screen.winfo_screenwidth()
 screenHeight = screen.winfo_screenheight()
@@ -14,20 +29,6 @@ screen.destroy()
 
 splash.resizable(False,False)
 splash.config(background="#ffeedb")
-splash.attributes('-fullscreen', True)
-splash
-
-
-def login_screen():
-    
-    splash.destroy()
-    
-    root=Tk()
-    root.title("Login")
-    # root.geometry(f"{screenWidth}x{screenHeight}")
-    root.resizable(True,True)
-    root.config(background="#ffeedb")
-    root.attributes('-fullscreen', True)
-    #Splash timer...
+splash.state("zoomed")
 splash.after(1000,login_screen)
 mainloop()
