3b4cc8847958331de994a9269245144aa1beae2c
ahmedessam228 <ahmedeliwa228@gmail.com>
2023-08-20 16:40:24 +0300

    ahmed

--- screens/client_reciept.py
+++ screens/client_reciept.py
@@ -34,19 +34,20 @@ logo.place(x=screenWidth - 100, y=15)
 
 mainFrame=CTkFrame(
     root,
-    fg_color=transparent,
+    fg_color=darkBlue,
     bg_color=transparent,
     corner_radius=20,
     width=200,
-    height=150
+    height=150,
+    
 )
 
 nameLabel = Label(
     mainFrame,
     text="Name : ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 nameLabel.grid(row=0, column=0 )
 
@@ -54,8 +55,8 @@ shownameLabel = Label(
     mainFrame,
     text="Ahmed Mamdouh ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 shownameLabel.grid(row=0, column=1 )
 
@@ -63,8 +64,8 @@ addressLabel = Label(
     mainFrame,
     text="address : ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 addressLabel.grid(row=1, column=0 )
 
@@ -72,8 +73,8 @@ showaddressLabel = Label(
     mainFrame,
     text="banha ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 showaddressLabel.grid(row=1, column=1 )
 
@@ -81,8 +82,8 @@ phoneLabel = Label(
     mainFrame,
     text="phone number : ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 phoneLabel.grid(row=2, column=0 )
 
@@ -90,8 +91,8 @@ showphoneLabel =Label(
     mainFrame,
     text="01146872172 ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 showphoneLabel.grid(row=2, column=1 )
 
@@ -99,8 +100,8 @@ priceLabel = Label(
     mainFrame,
     text="price : ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 priceLabel.grid(row=3, column=0 )
 
@@ -108,34 +109,33 @@ showpriceLabel = Label(
     mainFrame,
     text="160.5 ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 showpriceLabel.grid(row=3, column=1 )
 
 paid = Label(
     mainFrame,
     text='Paid  : ',
-    fg=black,
-    bg=beige,
-    font=(lucida, 20)
+    font=(lucida, 20),
+    fg=white,
+    bg=darkBlue,
 )
 paid.grid(row=4,column=0)
 
 showpaid = Label(
     mainFrame,
     text='200 ',
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
     font=(lucida, 20)
 )
 showpaid.grid(row=4,column=1)
 
 
-
 productFrame=CTkFrame(
     root,
-    fg_color=transparent,
+    fg_color=darkBlue,
     bg_color=transparent,
     corner_radius=20,
     width=200,
@@ -146,8 +146,8 @@ nameproductLabel = Label(
     productFrame,
     text="Name  ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 nameproductLabel.grid(row=0, column=0 )
 
@@ -155,8 +155,8 @@ shownameproductLabel = Label(
     productFrame,
     text="coffe ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 shownameproductLabel.grid(row=1, column= 0)
 
@@ -164,8 +164,8 @@ priceproductLabel = Label(
     productFrame,
     text="Price  ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 priceproductLabel.grid(row=0, column=1)
 
@@ -173,8 +173,8 @@ showpriceproductLabel = Label(
     productFrame,
     text="25",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 showpriceproductLabel.grid(row=1, column= 1)
 
@@ -182,8 +182,8 @@ countproductLabel = Label(
     productFrame,
     text=" Count ",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 countproductLabel.grid(row=0, column=2)
 
@@ -191,12 +191,12 @@ showpriceproductLabel = Label(
     productFrame,
     text="2",
     font=(lucida, 20),
-    fg=black,
-    bg=beige,
+    fg=white,
+    bg=darkBlue,
 )
 showpriceproductLabel.grid(row=1, column= 2)
 
-buttonnext = CTkButton(
+buttonPrint = CTkButton(
     root,
     width=250,
     height=50,
@@ -209,9 +209,9 @@ buttonnext = CTkButton(
     font=(lucida, 22),
     corner_radius=16,
 )
-buttonnext.place(x=500,y=600)
+buttonPrint.place(x=500,y=600)
 
-buttonnext = CTkButton(
+buttonReturn = CTkButton(
     root,
     width=250,
     height=50,
@@ -224,7 +224,7 @@ buttonnext = CTkButton(
     font=(lucida, 22),
     corner_radius=16,
 )
-buttonnext.place(x=800,y=600)
+buttonReturn.place(x=800,y=600)
 
 
 
