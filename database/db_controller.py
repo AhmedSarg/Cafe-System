8bb4d0a5d54955d4813a1351b2fef60b8b80c25b
AhmedSarg <xsarg22@gmail.com>
2023-08-22 01:31:37 +0300

    menu db

--- database/db_controller.py
+++ database/db_controller.py
@@ -1,4 +1,7 @@
 import sqlite3
+import sys
+sys.path.insert(0, "classes")
+from product import Product
 
 
 def openConnection():
@@ -21,3 +24,15 @@ def addClient(connection, client):
         (client.name, client.phone, client.address),
     )
     connection.commit()
+
+
+def getMenu(connection, category):
+    cursor = connection.cursor()
+    result = cursor.execute("select * from Product where category = ?", ([category])).fetchall()
+    products = list()
+    for product in result:
+        if product[0] != 0:
+            products.append(Product(id=product[0], name=product[1], category=product[2], price=product[3]))
+    connection.commit()
+    return products
+
